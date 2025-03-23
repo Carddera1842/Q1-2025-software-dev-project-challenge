7caf396aa333c1c16967f273dbb707c21d441cfb
Carddera1842 <carddera1842@gmail.com>
2025-03-23 12:35:25 -0500

    Setup for backend

---
+++ backend/Q1-2025/src/main/java/org/launchcode/Q1_2025/Q12025Application.java
@@ -0,0 +1,13 @@
+package org.launchcode.Q1_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q12025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q12025Application.class, args);
+	}
+
+}
---
+++ backend/Q1-2025/src/test/java/org/launchcode/Q1_2025/Q12025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Q1_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q12025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
